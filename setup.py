3d4f37a99c19f101f16df5d6049746a1d3af01c3
Eliel Garcia <inbox@eliel-garcia.com>
2023-03-28 21:03:08 -0400

    version change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pycfalias',
-    version='0.0.7',
+    version='0.0.8',
     author='Eliel Garcia',
     author_email='inbox@eliel-garcia.com',
     description='Simple Cloudflare email alias management',
